e3ca3106b19e4856a6ad4d24b458e32c983a998f
KAYSILVHA <kaysilvha@gmail.com>
2024-07-26 12:43:36 -0300

    ajustes e testes

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 import "./assets/style/style.scss"
 
 const Home = () => {
+    
     return (
         <div className='content'>
-            <div className="container-fluid div-container">Home</div>
-
+            <div className="container-fluid div-container">
+                <div className='home-title'>
+                    <h3>Olá</h3>
+                    <h4>Bem vindo(a)!</h4>
+                </div>
+                <div className='container-card'>
+                    <div class="card-slider d-flex">
+                        <div class="card">Card 1</div>
+                        <div class="card">Card 2</div>
+                        <div class="card">Card 3</div>
+                        <div class="card">Card 4</div>
+                        <div class="card">Card 5</div>
+                        <div class="card">Card 6</div>
+                        <div class="card">Card 7</div>
+                        <div class="card">Card 8</div>
+                        <div class="card">Card 9</div>
+                        <div class="card">Card 10</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
